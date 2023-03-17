a3094512eb48dfbe7ba9431c5589b468302992c7
ssw <taiyang0324@qq.com>
2023-03-17 11:35:17 +0800

    day2

--- 除法.cpp
+++ 除法.cpp
@@ -5,22 +5,32 @@ using namespace std;
 int main()
 {
 	string s1;
-	int n1;
-	cin>>s1;
-	cin>>n1;
+	long long n1;
+	cin >> s1;
+	cin >> n1;
 	vector<int> v;
-	int num=0;
-	for(int i=0;i<s1.size();i++)
+	long long num = 0;
+	if (n1 == 0||(s1.size()==1&&s1[0]==0))
 	{
-		num=num*10+(s1[i]-'0');
-		if(num>=n1)
+		return 0;
+	}
+	for (int i = 0; i < s1.size(); i++)
+	{
+		num = num * 10 + (s1[i] - '0');
+		v.push_back(num / n1);
+		num %= n1;
+	}
+	int flag = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (flag == 0 &&v[i] == 0)
 		{
-			v.push_back(num/n1);
-			num%=n1;
-		}	
+			continue;
+		}
+		else
+		{
+			cout << v[i];
+			flag = 1;
+		}
 	}
-	for(int i=0;i<v.size();i++)
-	{
-		cout<<v[i];
-	}
-} 
+}
