9f226e16bf23a6b2904a04de4a565aa45b3f1a77
2070super <1025404072@qq.com>
2023-04-01 10:53:20 +0800

    3.28

--- 车站.cpp
+++ 车站.cpp
@@ -3,22 +3,32 @@
 using namespace std;
 int main()
 {
-	int a,n,m,x;
-	cin>>a>>n>>m>>x;
-	if(x<=2)
+	int a, n, m, x;
+	cin >> a >> n >> m >> x;
+	if (x <= 2)
 	{
-		cout<<a;
+		cout << a;
 		return 0;
 	}
-	int a1[25],a2[25];
-	a1[2]=1;
-	a1[3]=2;
-	for(int i=4;i<n;i++)
+	if (x == n)
 	{
-		a1[i]=a1[i-1]+a1[i-2]-1;
-		a2[i]=a2[i-1]+a2[i-2]+1;
-	 } 
-	 int b=(m-a*a1[n-1])/a2[n-1];
-	 cout<<a*a1[x]+b*a2[x];
-	 return 0; 
- } 
+		cout << 0;
+		return 0;
+	}
+	int up[25], cot[25];
+	cot[1] = cot[2] =up[1] = a;
+	for (int j = 0; j <= m; j++)
+	{
+		up[2] = j;
+		for (int i = 3; i < n; i++)
+		{
+			up[i] = up[i - 1] + up[i - 2];
+			cot[i] = cot[i - 1] - up[i - 1] + up[i];
+		}
+		if (cot[n-1] == m)
+		{
+			cout << cot[x];
+			return 0;
+		}
+	}
+}
